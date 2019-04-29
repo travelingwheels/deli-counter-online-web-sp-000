956edf15e11ec569fe014f6c11b606351e05b40e
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-29 14:41:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,5 @@
 # Write your code here.
-def line(katz_deli)
- if katz_deli.length == 0 
-   return " The line is currently empty."
- else katz_deli.lenght >= 1 
-   return 
+katz_deli = []
+
+def line(x)
   
-end 
