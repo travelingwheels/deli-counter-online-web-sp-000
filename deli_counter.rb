a4341c12617d9bbfd0faa2968678c51d3c67dbcc
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-29 15:24:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ end
 
 def take_a_number(katz_deli,name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
