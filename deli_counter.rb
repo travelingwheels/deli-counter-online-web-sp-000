af24dc923b842c7e7d7b654eda8d033948e75b97
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-29 15:07:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,4 +9,4 @@ def line(x)
     x.each.with_index(1) do |name, index|
       line_array.push("#{index}. #{name}")
     end
-    puts "The line is currently: "
+    puts "The line is currently: #{line_array.join("
